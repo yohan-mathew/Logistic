34da786bbd3677da75f9c3bad2ce0c114955b2cd
Yohan Mathew <yohanmathew00@gmail.com>
2023-01-07 17:25:14 -0600

    login with layout, more changes on the way

--- Login.py
+++ Login.py
@@ -1,19 +1,37 @@
 from tkinter import *
+import time
+import sys
+ 
 root = Tk()
 root.title("Plate Data smpl 1.0")
-e1= Entry(root)
+root.geometry("400x150")
+
+frame1 = Frame(root)
+frame2 = Frame(root)
+frame3 = Frame(root)
+
+e1= Entry(frame2,width=50)
 
 def validateUser():
     
-    if e1.get() == "yohan":
-         Label(root, text="Welcome "+e1.get()).grid(row=3,column=0)
-    else:
-        Label(root, text="Sorry Access denied").grid(row=3,column=0)
+     if e1.get() == "yohan":
+          Label(root, text="Welcome "+e1.get()).pack()
+          root.update()
+     else:
+         rsltLbl = Label(root, text="Sorry Access denied")
+         rsltLbl.pack()
+         root.update()
+         time.sleep(2)
+         sys.exit()
+         
 
-Label(root, text="Login", padx="20",pady="20").grid(row=0,column=1,columnspan=3, )
-Label(root, text="Name").grid(row=1,column=0)
-e1.grid(row=1,column=1)
-Button(root, text="Login", command=validateUser).grid(row=2,column=1)
+frame1.pack(padx=10,pady=10)
+Label(frame1, text="Login",).pack()
+frame2.pack(padx=10,pady=5)
+Label(frame2, text="Name:").pack(side=LEFT)
+e1.pack()
+frame3.pack(padx=10,pady=1)
+Button(frame3, text="Login", command=validateUser).pack()
 
 
 root.mainloop()
