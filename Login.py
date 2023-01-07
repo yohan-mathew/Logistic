20dd6df7692b5f4a455e8ed7185932d73a499efe
Yohan Mathew <yohanmathew00@gmail.com>
2023-01-06 22:49:06 -0600

    added all the fields for login, now need to fix the layout.

--- Login.py
+++ Login.py
@@ -1,10 +1,19 @@
 from tkinter import *
 root = Tk()
 root.title("Plate Data smpl 1.0")
+e1= Entry(root)
 
-Label(root, )
+def validateUser():
+    
+    if e1.get() == "yohan":
+         Label(root, text="Welcome "+e1.get()).grid(row=3,column=0)
+    else:
+        Label(root, text="Sorry Access denied").grid(row=3,column=0)
 
-
+Label(root, text="Login", padx="20",pady="20").grid(row=0,column=1,columnspan=3, )
+Label(root, text="Name").grid(row=1,column=0)
+e1.grid(row=1,column=1)
+Button(root, text="Login", command=validateUser).grid(row=2,column=1)
 
 
 root.mainloop()
