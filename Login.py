3dd65bd4be8346e090a75d2ae8de311360db3e8a
Yohan Mathew <yohanmathew00@gmail.com>
2023-01-07 23:12:49 -0600

    added customTinker and gui has a second screen now

--- Login.py
+++ Login.py
@@ -1,37 +1,76 @@
 from tkinter import *
 import time
 import sys
- 
-root = Tk()
+import customtkinter as custom
+
+#created the Stage 
+root = custom.CTk()
 root.title("Plate Data smpl 1.0")
 root.geometry("400x150")
 
-frame1 = Frame(root)
-frame2 = Frame(root)
-frame3 = Frame(root)
 
-e1= Entry(frame2,width=50)
+#created all the frame(contains respective components)
+     #first page frames
+frame1 = custom.CTkFrame(root)
+frame2 = custom.CTkFrame(root)
+frame3 = custom.CTkFrame(root)
 
+
+#second page frames
+frame4 = custom.CTkFrame(root)
+
+#username
+usernametxt= custom.CTkEntry(frame2,width=50)
+
+# ---------------------------------------------------------------------------------------------
+#                                 **functions made to use everywhere**
+
+#removes any given frames from the stage so I can reuse the space
+def eraseContent(*frames):
+     for x in frames:
+          x.destroy()
+# ---------------------------------------------------------------------------------------------
+#                                 **functions made to use on frames**
+
+# Step2: after validation, get the details     
+def captureDetails():
+     frame4.pack()
+     plateNotxt = custom.CTkEntry(frame4)
+     plateWeighttxt = custom.CTkEntry(frame4)
+     plateNolbl = custom.CTkLabel(frame4,text="Plate Number: ")
+     plateWeightlbl = custom.CTkLabel(frame4,text="Plate Weight: ")
+     
+     plateNolbl.pack()
+     plateNotxt.pack()
+     
+     plateWeightlbl.pack()
+     plateWeighttxt.pack()
+# ---------------------------------------------------------------------------------------------
+
+# Step1: get username & validate user(IF user is in system, go to step 2, ELSE error & exit!)     
 def validateUser():
-    
-     if e1.get() == "yohan":
-          Label(root, text="Welcome "+e1.get()).pack()
+     if usernametxt.get() == "yohan":
+          custom.CTkLabel(root, text="Welcome " + usernametxt.get()).pack()
+          eraseContent(frame1,frame2,frame3)
           root.update()
+          captureDetails()
      else:
-         rsltLbl = Label(root, text="Sorry Access denied")
-         rsltLbl.pack()
-         root.update()
-         time.sleep(2)
-         sys.exit()
+          eraseContent(frame1,frame2,frame3)
+          rsltLbl = custom.CTkLabel(root, text="Sorry Access denied")
+          rsltLbl.pack()
+          root.update()
+          time.sleep(2)
+          sys.exit()
+# ---------------------------------------------------------------------------------------------
          
-
+# labels & frames for screen one 
 frame1.pack(padx=10,pady=10)
-Label(frame1, text="Login",).pack()
+custom.CTkLabel(frame1, text="Login",).pack()
 frame2.pack(padx=10,pady=5)
-Label(frame2, text="Name:").pack(side=LEFT)
-e1.pack()
+custom.CTkLabel(frame2, text="Name:").pack(side=LEFT)
+usernametxt.pack()
 frame3.pack(padx=10,pady=1)
-Button(frame3, text="Login", command=validateUser).pack()
+custom.CTkButton(frame3, text="Login", command=validateUser).pack()
 
 
 root.mainloop()
